ec81f6013d27420d04832760ec1d4b80b73fbc30
abhishekra07 <abhira719@gmail.com>
2023-02-03 10:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const { wrietToCsv } = require('./utils')
+const {
+    wrietToCsv
+} = require('./utils')
 
-app.use(bodyParser.urlencoded({ extended: false} ));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(bodyParser.json());
 
 app.post('/api/writeToCsv', async (req, res) => {
     await wrietToCsv(req.body);
-    res.status(200).json({message: 'Request Successfully added for Batch Processing!!!'})
+    res.status(200).json({
+        message: 'Request Successfully added for Batch Processing!!!'
+    })
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Batch App is running on port ${process.env.PORT || 3000}`)
+    console.log(`CSV Writer App is running on port ${process.env.PORT || 3000}`)
 })
