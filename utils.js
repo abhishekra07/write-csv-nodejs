9c4c9360fa9fbdf4533a97d7dbdb249f976539d3
abhishekra07 <abhira719@gmail.com>
2023-02-03 10:10:30 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,17 +4,29 @@ const path = require('path');
 
 const folderPath = 'files/CSV'
 
-const csvStream = csv.format({ headers: true });
+const csvStream = csv.format({
+    headers: true
+});
 
 
-exports.wrietToCsv = async ({ record }) => {
-    if(!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath, {recursive: true})
+exports.wrietToCsv = async ({
+    record
+}) => {
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, {
+            recursive: true
+        })
     }
     let writableStream = fs.createWriteStream(path.resolve(__dirname, 'files', 'CSV', 'employess.csv'));
     csvStream.pipe(writableStream);
-    for(const data of record) {
-        csvStream.write({ Name: data.name, Age: data.age, Position: data.position, Department: data.department, Experiance: data.exp });
+    for (const data of record) {
+        csvStream.write({
+            Name: data.name,
+            Age: data.age,
+            Position: data.position,
+            Department: data.department,
+            Experiance: data.exp
+        });
     }
     writableStream.close();
     csvStream.end();
